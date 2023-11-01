042e8a571d73f86071a9d75b1da595bb127bf2db
deinol18 <98658922+deinol18@users.noreply.github.com>
2023-10-31 22:53:50 -0600

    matriz 40x36

--- prueba 2.py
+++ prueba 2.py
@@ -5,7 +5,7 @@ ANCHO_VENTANA = 800
 ALTO_VENTANA = 720
 
 pygame.init()
-
+matriz = [[0] * 36 for _ in range(40)]
 
 class Bola:
     def __init__(self, ventana, x, y, radio, velocidad):
@@ -29,8 +29,8 @@ class Bola:
         if teclas[pygame.K_RIGHT]:
             x_siguiente += self.velocidad
 
-        if teclas[pygame.K_SPACE]:
-            print(int(self.x), int(self.y))
+        #if teclas[pygame.K_SPACE]:
+         #   print((int(self.x)//10)/2, (int(self.y)//10)/2)
 
 
         # Verificar colisión con los bordes de la ventana
@@ -69,11 +69,21 @@ class Juego:
         self.alto = alto
         self.ventana = pygame.display.set_mode((ancho, alto))
         pygame.display.set_caption("Mueve la bola")
+        self.imprimir_una_vez = True  # Variable para imprimir una sola vez
+        self.tecla_m_presionada = False  # Variable para rastrear el estado de la tecla M
 
-
+    def imprimir_matriz(self):
+        # Aquí debes definir la lógica para imprimir la matriz
+        # Por ejemplo, puedes iterar sobre la matriz y mostrar sus valores en la consola
+        if self.imprimir_una_vez:
+            for fila in range(40):
+                for columna in range(36):
+                    print(matriz[fila][columna], end=" ")
+                print()
+            self.imprimir_una_vez = False  # Restablecer la variable
 
     def ejecutar(self):
-        objetos_negros = [(0, 0, 800, 50), (0, 600, 800, 50), ]  # Ejemplo de objetos negros
+        objetos_negros = [(0, 0, 800, 50), (0, 670, 800, 50), (0, 0, 50, 720), (750, 0, 50, 720)]
 
         bola = Bola(self.ventana, 20, self.alto - 20, 20, 0.2)
 
@@ -85,10 +95,16 @@ class Juego:
 
             teclas = pygame.key.get_pressed()
 
+            if teclas[pygame.K_m] and not self.tecla_m_presionada:
+                self.imprimir_matriz()
+                print(f"Posición de la bola: ({bola.x}, {bola.y})")
+                self.tecla_m_presionada = True
+            elif not teclas[pygame.K_m]:
+                self.tecla_m_presionada = False
+
             bola.mover(teclas, objetos_negros)
 
             self.ventana.fill((0, 0, 128))
-
 
             # Dibujar objetos negros
             for objeto in objetos_negros:
@@ -98,9 +114,6 @@ class Juego:
 
             pygame.display.update()
 
-
-
 if __name__ == "__main__":
-
     juego = Juego(800, 720)
     juego.ejecutar()
